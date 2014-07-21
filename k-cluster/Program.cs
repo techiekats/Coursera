d4652eef14899989b62af624079f91686e23e5f2
khyati shah <khyati.a.shah@outlook.com>
2014-07-20 21:43:58 -0500

    k-clustering small set

---
+++ k-cluster/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace k_cluster
+{
+    class Program
+    {
+        static List<Tuple<int,int,int>> graph;
+        static List<List<int>> verticesIncluded;
+        static int nodeCount;
+        static void Main(string[] args)
+        {
+            LoadGraph();
+            Console.WriteLine("Max spacing=" + GetMaxSpacing(4));
+            Console.ReadKey();
+        }
+
+        private static void LoadGraph  ()
+        {
+            //test file op=134365
+            StreamReader graphFile = new StreamReader(@"C:\Users\khyati\documents\github\coursera\k-cluster\clusters.txt");
+            var t1 = from line in graphFile.Lines()
+                     let items = line.Split(' ')
+                     //where items.Length > 1
+                     select items.Length > 1 ? new { Weight = int.Parse(items[2]), Edge = new Tuple<int, int>(int.Parse(items[0]), int.Parse(items[1])) }
+                        : new {Weight = int.MinValue, Edge=new Tuple<int,int> (int.Parse (items[0]), int.Parse (items[0])) };
+            t1 = t1.OrderBy(t => t.Weight);
+            graph = new List<Tuple<int,int, int>>();
+            foreach (var e in t1)
+            {
+                 graph.Add (new Tuple<int,int,int>(e.Weight,e.Edge.Item1, e.Edge.Item2));
+            }
+            nodeCount = graph[0].Item2;
+            graph.RemoveAt(0);
+            verticesIncluded = new List<List<int>>();
+            Console.Write ("Graph size = " + graph.Count);
+        }
+        private static int GetMaxSpacing(short clusterSize)
+        {
+            UnionFind uf = new UnionFind(nodeCount);   
+            foreach (var g in graph)
+            {
+                /*uf.PrintClusters();
+                Console.WriteLine("--------------------");
+                Console.ReadKey();*/
+                if (uf.AreConnected(g.Item2, g.Item3))
+                {
+                    //Console.WriteLine(string.Format("{0} {1} are connected. Cluster size = {2}", g.Item2, g.Item3, uf.ClusterCount()));
+                    continue;
+                }
+                else
+                {
+                    if (uf.ClusterCount() == clusterSize)
+                    {
+                        return g.Item1;
+                    }
+                    
+                   // Console.WriteLine (string.Format ("Union of {0} {1}", g.Item2, g.Item3));
+                    uf.Union(g.Item2, g.Item3);
+                }
+            }
+            return -1;
+        }
+        
+
+    }
+    public static class StreamReaderSequence
+    {
+        public static IEnumerable<string> Lines(this StreamReader source)
+        {
+            String line;
+
+            if (source == null)
+                throw new ArgumentNullException("source");
+            while ((line = source.ReadLine()) != null)
+            {
+                yield return line;
+            }
+        }
+    }
+}
---
+++ k-cluster/UnionFind.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace k_cluster
+{
+    class UnionFind
+    {
+        private Dictionary<int, int> vertexLeaderMapping;
+        public UnionFind(int nodeCount)
+        {
+            Dictionary<int, int> load = new Dictionary<int, int>();
+            for (int i = 1; i <= nodeCount; i++)
+            {
+                load.Add(i, i);
+            }
+            vertexLeaderMapping = load;
+        }
+        public int Find(int node)
+        {
+            return vertexLeaderMapping.ContainsKey(node) ? vertexLeaderMapping[node] : int.MinValue;
+        }
+        public int ClusterCount()
+        {
+            return vertexLeaderMapping.Values.Distinct().Count();
+        }
+        public bool AreConnected(int node1, int node2)
+        {
+            return Find (node1) != int.MinValue && (Find(node1) == Find (node2));
+        }
+        public void Union(int node1, int node2)
+        {
+            int parent1 = Find(node1);
+            int parent2 = Find(node2);
+           /* bool aVertexToBeAdded = (parent1 == int.MinValue || parent2 == int.MinValue);
+            if (aVertexToBeAdded)
+            {
+                int parent = Math.Max(parent1, parent2);
+                if (parent == int.MinValue)//i.e. both are absent
+                {
+                    parent = node1;
+                }
+                if (vertexLeaderMapping.ContainsKey(node1))
+                {
+                    vertexLeaderMapping[node1] = parent;
+                }
+                else
+                {
+                    vertexLeaderMapping.Add(node1, parent);
+                }
+                if (vertexLeaderMapping.ContainsKey(node2))
+                {
+                    vertexLeaderMapping[node2] = parent;
+                }
+                else
+                {
+                    vertexLeaderMapping.Add(node2, parent);
+                }
+            }
+            else
+            {*/
+               if (parent1 != parent2)
+                {
+                    var ct1 = vertexLeaderMapping.Where(c1 => c1.Value == parent1);
+                    var ct2 = vertexLeaderMapping.Where(c2 => c2.Value == parent2);
+                    if (ct1.Count() <= ct2.Count())
+                    {
+                        var ls = ct1.ToArray<KeyValuePair<int, int>>();
+                        foreach (var b1 in ls)
+                        {
+                            vertexLeaderMapping[b1.Key] = parent2;
+                        }
+                    }
+                    else
+                    {
+                        var ls = ct2.ToArray<KeyValuePair<int, int>>();
+                        foreach ( var b2 in ls)
+                        {
+                            vertexLeaderMapping[b2.Key] = parent1;
+                        }
+                    }
+               // }
+            }
+        }
+        public void PrintClusters()
+        {
+            int[] distinctClusters = vertexLeaderMapping.Values.Distinct().ToArray<int>();
+            foreach (int c in distinctClusters)
+            {
+                Console.WriteLine("Cluster leader: " + c);
+                var subset =  vertexLeaderMapping.Where((k, v) => v == c).ToList<KeyValuePair<int,int>>();
+                foreach (var s in subset)
+                {
+                    Console.WriteLine(string.Format("{0}\t", s.Key));
+                }
+            }
+        }
+    }
+}
