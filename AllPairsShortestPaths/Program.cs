4d19cf402db13db13511b93ae3eac5b7c2fb4afe
khyati shah <khyati.a.shah@outlook.com>
2014-08-03 02:59:01 -0500

    finally got Bellman Ford correct

--- AllPairsShortestPaths/Program.cs
+++ AllPairsShortestPaths/Program.cs
@@ -9,21 +9,78 @@ namespace AllPairsShortestPaths
     class Program
     {
         private static string SOURCE = "0";
-
+        private static int EDGE_COUNT;
         private static Dictionary<string, int> computedDistances;
         private static Dictionary<string, List<Tuple<string, int>>> graph;
+        private static Dictionary<string, List<Tuple<string, int>>> incidentEdgesGraph;
         private static int INFINITY = int.MaxValue;
         static void Main(string[] args)
         {
-            LoadGraphFromTestData2();
-            ComputeDijkstraShortestPath();
-
+            // LoadGraphFromTestData2();
+            LoadDirectedTestGraph();
+            //ComputeDijkstraShortestPath();      
+            ComputeBellmanForShortestPaths();
             Console.WriteLine("Vertex\t\tDistance From Source");
             for (var gEnum = computedDistances.GetEnumerator(); gEnum.MoveNext(); )
             {
                 Console.WriteLine(string.Format("{0}\t\t\t{1}", gEnum.Current.Key, gEnum.Current.Value));
             }
             Console.ReadKey();
+        }
+
+
+        private static void LoadDirectedTestGraph ()
+        {
+            graph = new Dictionary<string, List<Tuple<string, int>>>();
+
+            //node 0
+            List<Tuple<string, int>> list0 = new List<Tuple<string, int>>();
+            list0.Add(new Tuple<String, int>("1", 2));
+            list0.Add(new Tuple<String, int>("2", 4));
+            graph.Add("0", list0);
+
+            //node 1
+            List<Tuple<string, int>> list1 = new List<Tuple<string, int>>();
+            list1.Add(new Tuple<String, int>("3", 2));
+            list1.Add(new Tuple<String, int>("2", 1));
+            graph.Add("1", list1);
+
+            //node 2
+            List<Tuple<string, int>> list2 = new List<Tuple<string, int>>();
+            list2.Add(new Tuple<String, int>("4", 4));
+            graph.Add("2", list2);
+
+            //node 3
+            List<Tuple<string, int>> list3 = new List<Tuple<string, int>>();
+            list3.Add(new Tuple<String, int>("4", 2));
+            graph.Add("3", list3);
+
+            //node 4
+            graph.Add("4", new List<Tuple<string, int>>());
+            EDGE_COUNT = 6;
+
+            incidentEdgesGraph = new Dictionary<string, List<Tuple<string, int>>>();
+            incidentEdgesGraph.Add("0", new List<Tuple<string, int>>());
+            List<Tuple<string, int>> _list1 = new List<Tuple<string, int>>();
+            _list1.Add(new Tuple<string, int>("0", 2));
+            incidentEdgesGraph.Add("1", _list1);
+
+            List<Tuple<string, int>> _list2 = new List<Tuple<string, int>>();
+            _list2.Add(new Tuple<string, int>("1", 1));
+            _list2.Add(new Tuple<string, int>("0", 4));
+            incidentEdgesGraph.Add("2", _list2);
+
+            List<Tuple<string, int>> _list3 = new List<Tuple<string, int>>();
+            _list3.Add(new Tuple<string, int>("1", 2));
+            incidentEdgesGraph.Add("3", _list3);
+
+            List<Tuple<string, int>> _list4 = new List<Tuple<string, int>>();
+            _list4.Add(new Tuple<string, int>("2", 4));
+            _list4.Add(new Tuple<string, int>("3", 2));
+            incidentEdgesGraph.Add("4", _list4);
+
+
+
         }
 
         /*EXPECTED OUTPUT:
@@ -38,7 +95,7 @@ namespace AllPairsShortestPaths
           7                8
           8                14
        * */
-        private static void LoadGraphFromTestData2()
+        private static void LoadUndirectedTestGraph()
         {
             graph = new Dictionary<string, List<Tuple<string, int>>>();
 
@@ -114,21 +171,19 @@ namespace AllPairsShortestPaths
 
             graph.Add("8", list8);
         }
-
         private static void ComputeDijkstraShortestPath()
         {
             String nextVertex = SOURCE;
-            HashSet<KeyValuePair<string, Tuple<bool, bool>>> computationStatus = new HashSet<KeyValuePair<string, Tuple<bool, bool>>>();
-            //Hashmap<String, <isProcessed, isReachable>>
+            Dictionary<string, Tuple<bool, bool>> computationStatus = new Dictionary<string,Tuple<bool,bool>>();
 
             //construct initial set and compute distances
             computedDistances = new Dictionary<string, int>();
 
             for (var k = graph.GetEnumerator(); k.MoveNext(); )
             {
-                String key = k.Current.Key;
+                string key = k.Current.Key;
                 computedDistances.Add(k.Current.Key, INFINITY);
-                computationStatus.Add(new KeyValuePair<string, Tuple<bool, bool>>(k.Current.Key, new Tuple<bool,bool>(false, false)));
+                computationStatus.Add(k.Current.Key, new Tuple<bool, bool>(false, false));
             }
             computedDistances[SOURCE] = 0;
             while (graph.Count != 0)
@@ -137,21 +192,17 @@ namespace AllPairsShortestPaths
                 int distanceOfCurrentVertex = computedDistances[nextVertex];
                 int minimum = INFINITY;
                 //update the graph with new calculations
-                if (graph.ContainsKey (nextVertex))
+                if (graph.ContainsKey(nextVertex))
                 {
-                    computationStatus.RemoveWhere(s => s.Key.Equals(nextVertex));
-                    computationStatus.Add(new KeyValuePair<string, Tuple<bool, bool>>(nextVertex, new Tuple<bool,bool>(true, true)));
-
+                    computationStatus [nextVertex] = new Tuple<bool, bool>(true, true);
                     //computationStatus.put(nextVertex, new Tuple<Boolean, Boolean>(true, true));
                     for (var distanceFromSource = graph[nextVertex].GetEnumerator(); distanceFromSource.MoveNext(); )
                     {
                         Tuple<String, int> t = distanceFromSource.Current;
-//                        computationStatus.put(t.x, new Tuple<Boolean, Boolean>(computationStatus.get(t.x).x, true));
-                        bool previouslyComputedValue = computationStatus.First(st=> st.Key == t.Item1).Value.Item1;
-                        computationStatus.RemoveWhere(s => s.Key.Equals(t.Item1));
-                        computationStatus.Add(new KeyValuePair<string, Tuple<bool, bool>>
-                            (t.Item1, new Tuple<bool, bool>(previouslyComputedValue, true)));
-                        
+                        //                        computationStatus.put(t.x, new Tuple<Boolean, Boolean>(computationStatus.get(t.x).x, true));
+                        bool previouslyComputedValue = computationStatus[t.Item1].Item1;
+                        computationStatus.Remove(t.Item1);
+                        computationStatus[t.Item1] = new Tuple<bool, bool>(previouslyComputedValue, true);
                         if (t.Item2 + distanceOfCurrentVertex < computedDistances[t.Item1])
                         {
                             computedDistances[t.Item1] = t.Item2 + distanceOfCurrentVertex;
@@ -173,48 +224,48 @@ namespace AllPairsShortestPaths
                         if (!entry.Value.Item1 && entry.Value.Item2 && computedDistances[entry.Key] < minimum)
                         {
                             nextVertex = entry.Key;
-                            minimum = computedDistances[entry.Key] ;
+                            minimum = computedDistances[entry.Key];
                         }
                     }
                 }
-
-                /*PRINT TEST OUTPUT
-                 * System.out.println("Node:"+ temp);
-                Iterator<Entry<String, int>> it = computedDistances.entrySet().iterator();
-                while (it.hasNext())
-                {
-                    Entry<String, int> entry = it.next();
-                    System.out.println("Node:" + entry.getKey() + "\tDistance:" + computedDistances.get(entry.getKey()) + "\n");
-                }
-                System.out.println("----------------------------------------------");*/
 
                 //remove the vertex from source
                 graph.Remove(temp);
             }
         }
-      
         private static void ComputeBellmanForShortestPaths()
         {
             int[][] matrix = new int [2][];
-            for (int temp = 0; temp < 2; temp++)
+            matrix[0] = new int[graph.Count];
+            matrix[1] = new int[graph.Count];
+            for (int index = 0; index < graph.Count; index ++ )
             {
-                matrix[temp] = new int[graph.Count];
-                Array.ForEach(matrix[temp], (a => a = 0)); //initialized all values to zero
+                matrix[1][index] = int.MaxValue;
             }
+            matrix[1][0] = 0;//source vertex
+            /*
+            foreach (var edge in graph["0"])
+            {
+                matrix[1][int.Parse(edge.Item1)] = edge.Item2;
+            }*/
+
             bool valueDecreasedInFinalIteration = false;
             for (int i = 0; i < graph.Count; i++)
             {
                 Array.Copy(matrix[1], matrix[0], graph.Count);
-                Array.ForEach(matrix[1], (a => a = int.MaxValue)); //initialized all values to zero
-
-                for (int v = 0; v < graph.Count; v++)
+                for (int index = 0; index < graph.Count; index++)
                 {
-                    int value1 = (i != 0) ? matrix[0][v] : int.MaxValue;
+                    matrix[1][index] = int.MaxValue;
+                }
+                for (int v = 0; v < graph.Count;  v++)
+                {
+                    int value1 =  matrix[0][v];
 
                     int value2 = int.MaxValue;
-                    foreach (int t in matrix[0])
+                    foreach ( var edge in incidentEdgesGraph[v.ToString()])
                     {
-                        int temp = t + computedDistances[t.ToString()];
+                        int temp = matrix[0][int.Parse(edge.Item1)] == int.MaxValue ? int.MaxValue //to avoid overflow
+                            :matrix[0][int.Parse(edge.Item1)] + edge.Item2;
                         value2 = temp < value2 ? temp : value2;
                     }
                     matrix[1][v] = Math.Min(value1, value2);
@@ -224,6 +275,12 @@ namespace AllPairsShortestPaths
             {
                 Console.WriteLine("Graph has negative edges");
             }
+            computedDistances = new Dictionary<string,int>();
+            for (int i=0; i < graph.Count(); i++)
+            {
+                computedDistances.Add(i.ToString(), matrix[1][i]);
+            }
+             
         }
     }
 }
